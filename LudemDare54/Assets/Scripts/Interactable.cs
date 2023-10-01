feb742a31d8ce8f12b53b736afd44523e582b2c8
BenParisi <benparisi90@gmail.com>
2023-09-30 23:11:23 -0600

    interactable items can have an editable position from which the player should interact with them

--- LudemDare54/Assets/Scripts/Interactable.cs
+++ LudemDare54/Assets/Scripts/Interactable.cs
@@ -2,11 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class Interactable : MonoBehaviour
 {
     public float interactDistance = 1f;
     public Action Interact;
+
+    [SerializeField] Vector3 interactionOffset;
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position + interactionOffset, 0.1f);
+        interactionOffset = Handles.PositionHandle(transform.position + interactionOffset, Quaternion.identity) - transform.position;
+    }
+#endif
 
     //if I am clicked on, say hello
     void OnMouseDown()
@@ -16,10 +30,9 @@ public class Interactable : MonoBehaviour
             return;
         }
         //if the player is farther than the interact dictance
-        if (Vector3.Distance(transform.position, PlayerController.instance.Character.transform.position) > interactDistance)
+        if (Vector3.Distance(transform.position + interactionOffset, PlayerController.instance.Character.transform.position) > interactDistance)
         {
-            Vector3 targetDestination = transform.position;
-            Vector3 closestPoint = LocationManager.instance.CurrentLocation.walkable.ClosestPoint(targetDestination);
+            Vector3 closestPoint = LocationManager.instance.CurrentLocation.walkable.ClosestPoint(transform.position + interactionOffset);
             PlayerController.instance.Character.Move(closestPoint, true);
             PlayerController.instance.interactOnReachDestination = this;
         }
